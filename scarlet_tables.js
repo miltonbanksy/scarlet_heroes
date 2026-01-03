43a80d7fa65ea161e6c3791e1d236083b99a1a87
miltonbanksy <miltonbanksy@gmail.com>
2026-01-04 00:48:29 +0900

    Added more table entries

--- scarlet_tables.js
+++ scarlet_tables.js
@@ -1,18 +1,28 @@
 const scarlet_dungeons = [
     {
         type: "cavern",
-        subtypes: ["abandoned mine", "bandit hideout"],
-        locations: ["caved-in cavern", "circular chamber"]
+        subtypes: ["abandoned mine", "bandit hideout", "beast lair", "cave village", "astrological intersection", "hermit's haven", "massacre site", "prehistoric dwelling", "prehuman shrine", "refugee shelter", "refuge", "unquiet grave"],
+        locations: ["caved-in cavern", "circular chamber", "crumbling dry cave", "dead end", "domed cavern", "flowing water", "narrow crevasse", "natural bridge", "pit ledge", "sloping gallery", "spiral tunnel", "stalactite maze", "strata pocket", "stream bank","tunnel intersection","twisted tunnel","underground pool","vertical shaft","water-smoothed cave","wide gallery"]
     },
     {
         type: "habitation",
-        subtypes: ["bandit camp", "crumbling villa"],
-        locations: ["bathing room", "cellar"]
+        subtypes: ["bandit camp", "crumbling villa", "clanhold", "escaped slave camp", "exile hamlet", "farming village", "hunter's camp", "non-human settlement", "pillaged village", "prehuman habitation", "ruined manor", "sacked town"],
+        locations: ["bathing room", "cellar", "courtyard", "dining room", "garden", "great  hall", "kennel / stable", "kitchen", "library", "master bedroom", "pantry", "salon", "scullery", "servant's rooms", "shrine", "solar", "stairwell", "storeroom", "study", "vault"]
     },
     {
         type: "fortress",
-        subtypes: ["abandoned army camp", "brigand stronghold"],
-        locations: ["archery range", "armory"]
+        subtypes: ["abandoned army camp", "brigand stronghold", "broken keep", "crumbling watchtower", "decaying toll keep", "defensible tower", "fortified manor", "natural fortress", "prehuman fort", "remnant dungeon", "ruined caravanserai", "shattered outpost"],
+        locations: ["archery range", "armory", "barracks", "cemetery", "cistern", "commissary", "family quarters", "gatehouse", "guardpost", "kitchen", "map and file room", "mess hall", "officer's quarters", "prison cell", "sickroom", "smithy", "stables", "storeroom", "training hall", "vault"]
+    },
+    {
+        type: "temple",
+        subtypes: ["alien place of worship", "cursed temple", "forgotten fane", "fortified abbey", "heretic hideout", "holy natural feature", "isolated monastery", "pilgrimage temple", "prehuman shrine", "ruined nunnery", "sacred tomb", "sectarian stronghold"],
+        locations: ["acolyte's cells", "altar hall", "bathing room", "cemetery", "garden", "guardpost", "kitchen", "library", "meditation room", "minor shrine", "pilgrim quarters", "prayer hall", "priest's quarters", "relic chamber", "ritual chamber", "sickroom", "storeroom", "vault", "vestry", "workroom"]
+    },
+    {
+        type: "academy",
+        subtypes: ["cabal of infernalists", "demon-ruled school", "fortified academy", "foul breeding pit", "inhuman wizard lair", "lost library", "mad wizard's lab", "necromancer cell", "plagued research lab", "prison for mages", "ruined mage school", "wizard tower"],
+        locations: ["laboratory", "occult library", "apprentice rooms", "msater bedroom", "summoning room", "divination room", "storeroom", "kitchen", "study hall", "dining hall", "vault", "secure cell", "servant's chambers", "crafter's workshop", "academy archives", "memorial hall", "monument", "couryard / garden", "stables", "lecture hall"]
     }
 ];
 
@@ -21,4 +31,15 @@ const scarlet_dungeons_size = [
     {size: () => rollxdx(2, 6)},
     {size: () => rollxdx(3, 6)},
     {size: () => rollxdx(1, 10) + 10},
+    {size: () => rollxdx(1, 10) + 5},
+    {size: () => rollxdx(3, 6)},
+    {size: () => rollxdx(1, 20)},
+    {size: () => rollxdx(2, 12)},
+    {size: () => rollxdx(1, 20)},
+    {size: () => rollxdx(1, 12) + 10},
+    {size: () => rollxdx(2, 20)},
+    {size: () => rollxdx(1, 100)},
 ];
+
+const scarlet_dungeons_inhabitants = ["bandits", "deserters", "dwarf repenters", "ghosts", "shou", "elf zealots", "lizardfolk", "ogres", "wild beasts", "tidespawn", "tide cult", "centipede women", "necromancers", "infernalists", "rebels", "heretics", "looters", "supremacists", "abominations", "degenerates"];
+const scarlet_dungeons_inhabitants_rank = ["minion", "elite", "boss", "mage", "civilian"];
--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 const btnDungeonType = document.getElementById('btn-dungeon-type');
 const displayDungeonType = document.getElementById('display-dungeon-type');
+const btnRoomType = document.getElementById('btn-room-type');
+const displayRoomType = document.getElementById('display-room-type');
+
+let dungeon_type = "";
 
 // // LISTENERS 
+
 btnDungeonType.addEventListener('click', () => {
-    getDungeonType();
+    displayRoomType.innerHTML = "";
+    
+    // Get dungeon type & subtype
+    const type = Math.floor(Math.random() * scarlet_dungeons.length);
+    dungeon_type = scarlet_dungeons[type];
+    
+    const dungeon_subtype_index = Math.floor(Math.random() * dungeon_type.subtypes.length);
+    const dungeon_subtype = dungeon_type.subtypes[dungeon_subtype_index];
+
+    // Get dungeon size
+    const row = Math.floor(Math.random() * scarlet_dungeons_size.length);
+    const dungeon_size = scarlet_dungeons_size[row].size();
+
+    // Get dungeon inhabitants
+    const dungeon_inhabitants_index = Math.floor(Math.random() * scarlet_dungeons_inhabitants.length);
+    const dungeon_inhabitants = scarlet_dungeons_inhabitants[dungeon_inhabitants_index];
+
+    displayDungeonType.innerHTML = `${dungeon_type.type}, ${dungeon_subtype}, ${dungeon_size} locations, ${dungeon_inhabitants}`;
+});
+
+
+btnRoomType.addEventListener('click', () => {
+    if ( dungeon_type == "") {
+        return;
+    }
+
+    const room_type_index = Math.floor(Math.random() * dungeon_type.locations.length);
+    const room_type = dungeon_type.locations[room_type_index];
+    displayRoomType.innerHTML = room_type;
 });
 
 // // HELPER FUNCTIONS
@@ -20,23 +53,4 @@ function rollxdx(number_of_dice, size_of_dice) {
         dice_pool.push(die);
     }
     return sumDicePool(dice_pool);
-};
-
-// // FUNCTIONS
-
-function getDungeonType() {
-    const type = Math.floor(Math.random() * scarlet_dungeons.length);
-    const dungeon_type = scarlet_dungeons[type];
-    
-    const subtype = Math.floor(Math.random() * dungeon_type.subtypes.length);
-    const dungeon_subtype = scarlet_dungeons[type].subtypes[subtype];
-
-    const dungeon_size = getDungeonSize();
-
-    displayDungeonType.innerHTML = `${dungeon_type.type}, ${dungeon_subtype}, ${dungeon_size} locations`;
-};
-
-function getDungeonSize() {
-    const row = Math.floor(Math.random() * scarlet_dungeons_size.length);
-    return scarlet_dungeons_size[row].size();
 };
